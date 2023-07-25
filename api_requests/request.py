2e8e5643c1ce32a41027d5cb4d7c610cbb6e8f58
Ruslan <ruslan.marsikcat@yandex.ru>
2023-07-25 16:29:32 +0300

    Создал структуру под базу данных

--- api_requests/request.py
+++ api_requests/request.py
@@ -2,11 +2,11 @@ import json
 
 import requests
 
-import config
+from settings import api_config
 
 
 def get_city_coord(city: str) -> str:
-    payload = {"geocode": city, "apikey": config.geo_key, "format": "json"}  # параметры запроса
+    payload = {"geocode": city, "apikey": api_config.geo_key, "format": "json"}  # параметры запроса
     r = requests.get("https://geocode-maps.yandex.ru/1.x", params=payload)
     geo = json.loads(r.text)
     return geo["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["Point"]["pos"]
@@ -15,7 +15,7 @@ def get_city_coord(city: str) -> str:
 def get_weather(city: str) -> str:
     coords = get_city_coord(city).split()
     payload = {"lon": coords[0], "lat": coords[1], "lang": "ru_RU"}
-    r = requests.get("https://api.weather.yandex.ru/v2/forecast", params=payload, headers=config.weather_key)
+    r = requests.get("https://api.weather.yandex.ru/v2/forecast", params=payload, headers=api_config.weather_key)
     weather_data = json.loads(r.text)
     return weather_data["fact"]
 
