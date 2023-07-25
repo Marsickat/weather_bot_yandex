56b6ee9732177c76504e21efd44ffa2a0c437034
Ruslan <ruslan.marsikcat@yandex.ru>
2023-07-25 11:30:56 +0300

    Распарсил информацию о городе
    Написал функцию для вывода информации о фактической погоде для интересующего города

--- request.py
+++ request.py
@@ -5,11 +5,19 @@ import requests
 import config
 
 
-def get_city_coord(city):
+def get_city_coord(city: str) -> str:
     payload = {"geocode": city, "apikey": config.geo_key, "format": "json"}  # параметры запроса
     r = requests.get("https://geocode-maps.yandex.ru/1.x", params=payload)
     geo = json.loads(r.text)
     return geo["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["Point"]["pos"]
 
 
-print(get_city_coord("Ставрополь"))
+def get_weather(city: str) -> str:
+    coords = get_city_coord(city).split()
+    payload = {"lon": coords[0], "lat": coords[1], "lang": "ru_RU"}
+    r = requests.get("https://api.weather.yandex.ru/v2/forecast", params=payload, headers=config.weather_key)
+    weather_data = json.loads(r.text)
+    return weather_data["fact"]
+
+
+print(get_weather("Ставрополь"))
