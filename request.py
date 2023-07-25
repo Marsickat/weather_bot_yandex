4116ba932da4382c4dcb06ae26f4fdbfc3f59e1e
Ruslan <ruslan.marsikcat@yandex.ru>
2023-07-25 08:02:53 +0300

    init

---
+++ request.py
@@ -0,0 +1,14 @@
+import json
+
+import requests
+
+import config
+
+
+def get_city_coord(city):
+    payload = {"geocode": city, "apikey": config.geo_key, "format": "json"}  # параметры запроса
+    r = requests.get("https://geocode-maps.yandex.ru/1.x", params=payload)
+    print(r)
+
+
+get_city_coord("Ставрополь")
