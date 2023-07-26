427d299e02699bce34200871ad7403ec55c87b4e
Ruslan <ruslan.marsikcat@yandex.ru>
2023-07-26 08:09:38 +0300

    Добавил небольшой тестовый функционал

--- bot.py
+++ bot.py
@@ -18,5 +18,26 @@ async def start_message(message: types.Message):
     await message.answer(text, reply_markup=markup)
 
 
+@dp.message_handler(regexp="Погода в моём городе")
+async def get_user_city_weather(message: types.Message):
+    markup = types.reply_keyboard.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+    btn1 = types.KeyboardButton("Меню")
+    markup.add(btn1)
+    text = "Я пока так не умею"
+    await message.answer(text, reply_markup=markup)
+
+
+@dp.message_handler(regexp="Меню")
+async def start_message(message: types.Message):
+    markup = types.reply_keyboard.ReplyKeyboardMarkup(row_width=2)
+    btn1 = types.KeyboardButton("Погода в моём городе")
+    btn2 = types.KeyboardButton("Погода в другом месте")
+    btn3 = types.KeyboardButton("История")
+    btn4 = types.KeyboardButton("Установить свой город")
+    markup.add(btn1, btn2, btn3, btn4)
+    text = f"Привет {message.from_user.first_name}, я бот, который расскажет тебе о погоде на сегодня"
+    await message.answer(text, reply_markup=markup)
+
+
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
