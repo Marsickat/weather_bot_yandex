d5d727faa31f3485cef150b5e89682b7dca9ef91
Ruslan <ruslan.marsikcat@yandex.ru>
2023-07-27 16:07:10 +0300

    Исправил комментарий

--- settings/api_config.py
+++ settings/api_config.py
@@ -1,2 +1,2 @@
 geo_key = ""  # тут нужно вписать "JavaScript API и HTTP Геокодер"
-weather_key = {"X-Yandex-API-Key": ""}  # тут нужно вписать в качестве ключа "API Яндекс.Погоды"
+weather_key = {"X-Yandex-API-Key": ""}  # тут нужно вписать в качестве значения "API Яндекс.Погоды"
